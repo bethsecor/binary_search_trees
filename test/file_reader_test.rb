173aaddc3e3dc16e31b5400efcc317950e5a7a76
Beth Secor <elizabeth.a.secor@gmail.com>
2015-10-25 18:31:12 -0600

    Added tests for FileReader and FileWriter

--- test/file_reader_test.rb
+++ test/file_reader_test.rb
@@ -1,4 +1,4 @@
-# ruby ./test/file_reader_test.rb message.txt braille.txt
+# ruby ./test/file_reader_test.rb read_test_file.txt
 
 gem 'minitest', '~> 5.0'
 require 'minitest/autorun'
@@ -6,8 +6,13 @@ require 'minitest/pride'
 require_relative '../lib/file_reader'
 
 class FileReaderTest < Minitest::Test
-  def test_file_reader_reads
+  def test_file_reader_exists
     reader = FileReader.new
     assert reader.respond_to?(:read)
+  end
+
+  def test_file_reader_reads
+    reader = FileReader.new
+    assert_equal "Hello World!\n", reader.read
   end
 end
--- test/file_writer_test.rb
+++ test/file_writer_test.rb
@@ -1,13 +1,22 @@
-# ruby ./test/file_writer_test.rb message.txt braille.txt
+# ruby ./test/file_writer_test.rb write_test_file.txt write_test_file.txt
 
 gem 'minitest', '~> 5.0'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/file_writer'
+require_relative '../lib/file_reader'
 
 class FileWriterTest < Minitest::Test
-  def test_file_reader_reads
+  def test_file_writer_exists
     writer = FileWriter.new
     assert writer.respond_to?(:write)
+  end
+
+  def test_file_writer_writes
+    writer = FileWriter.new
+    text = "Bonjour!"
+    writer.write(text)
+    reader = FileReader.new
+    assert "Bonjour!\n", reader.read
   end
 end
